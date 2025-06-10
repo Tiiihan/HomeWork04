495762d620d0e4f87a4fc7c0ba7058a6b65e82e1
Yuliia Tykhovska <uliatihovska1@gmail.com>
2025-06-10 17:52:14 +0300

    Added realisation for task 4.1

--- HomeWork04/HomeWork04/Program.cs
+++ HomeWork04/HomeWork04/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿a: Console.Write("Enter number of the day of the week: ");
+
+switch (Console.ReadLine())
+{
+	case "1":
+		Console.WriteLine("It`s Monday");
+		break;
+	case "2":
+		Console.WriteLine("It`s Tuesday");
+		break;
+	case "3":
+		Console.WriteLine("It`s Wednesday");
+		break;
+	case "4":
+		Console.WriteLine("It`s Thursday");
+		break;
+	case "5":
+		Console.WriteLine("It`s Friday");
+		break;
+	case "6":
+		Console.WriteLine("It`s Saturday");
+		break;
+	case "7":
+		Console.WriteLine("It`s Sunday");
+		break;
+	default:
+		Console.WriteLine("Please, enter number from 1 to 7");
+		goto a;
+}
