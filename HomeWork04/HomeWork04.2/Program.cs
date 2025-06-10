6d8b3cfcd6671602b0846e5034c92f0d85dfabcc
Yuliia Tykhovska <uliatihovska1@gmail.com>
2025-06-10 18:44:59 +0300

    Added realisation for task 4.2

--- HomeWork04/HomeWork04.2/Program.cs
+++ HomeWork04/HomeWork04.2/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿beggining:
+//Initialisation variables and enter data
+Console.Write("Enter first integet number: ");
+bool isInt1 = Int32.TryParse(Console.ReadLine(), out int firstNumber);
+
+Console.Write("Enter second integer number: ");
+bool isInt2 = Int32.TryParse(Console.ReadLine(),out int secondNumber);
+
+enteringOperation:
+Console.Write("Enter mathematical operation (* , / , + , -): ");
+string operation = Console.ReadLine();
+
+//Check if data is correct
+if (!(isInt1 && isInt2))
+{
+    Console.WriteLine("\nPlease, enter correct data\n");
+	goto beggining;
+}
+
+//Do calculation
+switch (operation)
+{
+	case "+":
+        Console.WriteLine($"\n{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+		break;
+	case "-":
+		Console.WriteLine($"\n{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
+		break;
+	case "*":
+		Console.WriteLine($"\n{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+		break;
+	case "/" when secondNumber != 0:
+		Console.WriteLine($"\n{firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
+		break;
+	case "/" when secondNumber == 0:
+		Console.WriteLine("\nOops, maybe you want to divide by zero.\nPlease, enter correct data\n");
+		goto beggining;
+	default:
+        Console.WriteLine("\nOops, maybe you entered wrong operation.\nPlease, enter correct data\n");
+		goto enteringOperation;
+}
